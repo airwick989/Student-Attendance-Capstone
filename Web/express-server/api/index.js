022088d0be813d32fffb46603c0caecb05afd91e
airwick989 <73313597+airwick989@users.noreply.github.com>
2023-10-26 16:26:27 -0400

    Implemented a simple express.js server

---
+++ Web/express-server/api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Use the specified port or default to 3000
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
