ce8f9f711454878112a0d120bd1b991ce02925be
strato67 <saaranshsharma28@gmail.com>
2023-10-27 17:09:43 -0400

    middleware reply back with student

--- Web/express-server/middleware/classroom.js
+++ Web/express-server/middleware/classroom.js
@@ -1,22 +1,15 @@
 const studentSignIn = async (req, res) =>{
 
     try{
-        res.status(200).json({message: "success"});
-    }catch{
-        res.status(400).json({error: "error"})
-    }
 
+        const {studentID, preferredName, pronouns, seatNumber } = req.body;
+        
+
+        res.status(200).json(req.body);
+    }catch (e){
+        res.status(400).json({error: e.message})
+    }
 
 }
 
-
-
 module.exports = {studentSignIn};
-
-
-
-
-
-
-
-
