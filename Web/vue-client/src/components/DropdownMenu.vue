96ff7b49da94820092bc30a8b2554860d7fb3ca4
Gollum6578 <joshua.white@ontariotechu.net>
2023-11-04 15:06:39 -0400

    Made adjustments to the component libarary
    added the props for hover state student id and name, added user input line, started on custom drop down menu.

---
+++ Web/vue-client/src/components/DropdownMenu.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="custom-dropdown">
+      <div class="selected-option"><div class="text-wraper">here</div></div>
+        <div class="items">
+          <div class="option">
+            {{ option }}
+          </div>
+      </div>
+      
+    </div>
+  </template>
+                               
+  <script>
+  export default {
+     name: "CustomDropdown",
+     props:{
+        title:{
+          type: String,
+          default: "",
+        },
+        options:{
+          type: Array,
+          required: true,
+        },
+      }
+  };
+  </script>
+                               
+  <style>
+  .selected-option{
+    background-color: #1f1f1f;
+    border-radius: 10px;
+    position: relative;
+    height: 74px;
+    width: 325px;
+  }
+  .text-wraper{
+    padding: 14px 0 0 5px;
+    font-family: "Inter-Regular", Helvetica;
+    color: #ffffff;
+    font-size: 36px;
+    letter-spacing: 0;
+    line-height: normal;
+    white-space: nowrap;
+  }
+  </style>   
+  
---
+++ Web/vue-client/src/components/UserInputLine.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="user-input-line">
+      <div class="text-wrapper">Email</div>
+      <hr class="email-line"/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "UserInputLine",
+    props:{
+      propmt:{
+        type: String,
+        default: "",
+      },
+      input:{
+        type: String,
+        default: "",
+      },
+    }
+  };
+  </script>
+  
+  <style>
+  .user-input-line {
+    height: 40px;
+    position: relative;
+    width: 450px;
+  }
+  
+  .user-input-line .email-line {
+    border: 1px solid;
+    border-color: #ffffff;
+    left: 0;
+    position: relative;
+    width: 450px;
+  }
+  
+  .user-input-line .text-wrapper {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: relative;
+    white-space: nowrap;
+    width: 450px;
+    margin-bottom: -12px;
+  }
+  </style>
+  
+  
