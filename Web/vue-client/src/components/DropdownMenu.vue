71dc43bb25581ff03ee225a3858cb455dd4512f5
Gollum6578 <joshua.white@ontariotechu.net>
2023-11-08 14:29:54 -0500

    Update DropdownMenu.vue
    Added drop down display block

--- Web/vue-client/src/components/DropdownMenu.vue
+++ Web/vue-client/src/components/DropdownMenu.vue
@@ -1,47 +1,83 @@
 <template>
-    <div class="custom-dropdown">
-      <div class="selected-option"><div class="text-wraper">here</div></div>
-        <div class="items">
-          <div class="option">
-            {{ option }}
-          </div>
-      </div>
-      
+  <div class="custom-dropdown">
+    <div class="selected-option"><div class="text-wraper">{{title}}</div></div>
+      <div class="drop-down-menu">
+        <div class="option">
+         {{options}}
+        </div>
     </div>
-  </template>
-                               
-  <script>
-  export default {
-     name: "CustomDropdown",
-     props:{
-        title:{
-          type: String,
-          default: "",
-        },
-        options:{
-          type: Array,
-          required: true,
-        },
-      }
-  };
-  </script>
-                               
-  <style>
-  .selected-option{
-    background-color: #1f1f1f;
-    border-radius: 10px;
-    position: relative;
-    height: 74px;
-    width: 325px;
-  }
-  .text-wraper{
-    padding: 14px 0 0 5px;
-    font-family: "Inter-Regular", Helvetica;
-    color: #ffffff;
-    font-size: 36px;
-    letter-spacing: 0;
-    line-height: normal;
-    white-space: nowrap;
-  }
-  </style>   
-  
+    
+  </div>
+</template>
+                             
+<script>
+export default {
+   name: "CustomDropdown",
+   props:{
+      title:{
+        type: String,
+        default: "",
+      },
+      options:{
+        type: Array,
+        required: true,
+      },
+    }
+};
+</script>
+                             
+<style>
+.selected-option{
+  background-color: #202020;
+  border-radius: 10px;
+  position: relative;
+  height: 74px;
+  width: 325px;
+}
+.text-wraper{
+  padding: 14px 0 0 5px;
+  font-family: "Inter-Regular", Helvetica;
+  color: #ffffff;
+  font-size: 36px;
+  letter-spacing: 0;
+  line-height: normal;
+  white-space: nowrap;
+}
+.drop-down-menu{
+  position: relative;
+  width: 280px;
+  height: 175px;
+  background-color: #313131;
+  opacity: 0.95;
+  border-radius: 0 0 25px 25px;
+  margin-left: 36px;
+  overflow-y: scroll;
+}
+.option{
+  top: 7px;
+  left: 18px;
+  position: absolute;
+  font-family: "Inter-Regular", Helvetica;
+  font-weight: 400;
+  color: #ffffff;
+  font-size: 36px;
+}
+
+body::-webkit-scrollbar {
+  width: 14px;               /* width of the entire scrollbar */
+}
+
+body::-webkit-scrollbar-track {
+  background: #D9D9D9;        /* color of the tracking area */
+}
+body::-webkit-scrollbar-thumb {
+  background-color: #8781AD;    /* color of the scroll thumb */
+  border-radius: 5px;       /* roundness of the scroll thumb */
+  border: 1px solid #D9D9D9;  /* creates padding around scroll thumb */
+}
+body {
+  scrollbar-width: thin;          /* "auto" or "thin" firefox */
+  scrollbar-color: #8781AD #D9D9D9;   /* scroll thumb and track firefox*/ 
+}
+</style>   
+
