3288e25cb01d381cdc3215ef3a6d84baf7f3bc46
Saaransh Sharma <saaranshsharma28@gmail.com>
2023-11-24 10:59:43 -0500

    end class routes

--- Web/express-server/middleware/professorFunctions.js
+++ Web/express-server/middleware/professorFunctions.js
@@ -1,15 +1,25 @@
 const classroomScan = async (req, res) => {
-    try {
-      const classroom  = req.params.id;
-  
-      const seatController = await import("../database/seatController.mjs");
-      const response = await seatController.getAllSeats(classroom);
-  
-      res.status(200).json({ response });
-    } catch (e) {
-      res.status(400).json({ error: e.message });
-    }
-  };
+  try {
+    const classroom = req.params.id;
 
+    const seatController = await import("../database/seatController.mjs");
+    const response = await seatController.getAllSeats(classroom);
 
-  module.exports = { classroomScan };
+    res.status(200).json({ response });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+const endClass = async () => {
+  try {
+    const classroom = req.params.id;
+    const seatController = await import("../database/seatController.mjs");
+    
+
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+module.exports = { classroomScan, endClass };
--- Web/express-server/routes/professorRoutes.js
+++ Web/express-server/routes/professorRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {
-    classroomScan
+    classroomScan, endClass
 } = require("../middleware/professorFunctions");
 
 router.get("/classList/:id", classroomScan );
+router.patch("/:id/end", endClass);
+
 
 module.exports = router;
