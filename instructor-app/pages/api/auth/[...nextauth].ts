dc021951709c3457f265e91143d0f55fa2c766c5
strato67 <saaranshsharma28@gmail.com>
2023-12-19 20:45:07 -0500

    setting up firebase adapter for future use

--- instructor-app/pages/api/auth/[...nextauth].ts
+++ instructor-app/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter"
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { cert } from "firebase-admin/app"
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -10,6 +12,13 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  // Need to add Firestore for this
+
+  /*adapter: FirestoreAdapter({credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g,'\n'),
+  })})*/
 }
 
 export default NextAuth(authOptions)
