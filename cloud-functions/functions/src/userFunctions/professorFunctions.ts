3192ea18800a3b38c20e08ae30c4f4c6d6271014
strato67 <saaranshsharma28@gmail.com>
2024-01-30 19:29:19 -0500

    setting up busboy for csv upload

--- cloud-functions/functions/src/userFunctions/professorFunctions.ts
+++ cloud-functions/functions/src/userFunctions/professorFunctions.ts
@@ -2,6 +2,7 @@
 import * as express from "express";
 import * as roomController from "../controllers/roomController";
 import * as courseController from "../controllers/courseController";
+import * as busboy from "busboy";
 
 // Room Functions
 export const getAllRoomNames = async (
@@ -65,17 +66,60 @@ export const createNewCourse = async (
   req: express.Request,
   res: express.Response
 ) => {
-  const {courseCode, courseName, room} = req.body;
-  try {
-    const response = await courseController.createCourse(
-      courseCode,
-      courseName,
-      room
-    );
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+  const bb = busboy({headers: req.headers});
+
+  const formData: any = {};
+
+  bb.on("field", (fieldname, val) => {
+    formData[fieldname] = val;
+  });
+
+  bb.on(
+    "file",
+    (
+      fieldname: any,
+      file: any,
+      filename: any,
+      encoding: any,
+      mimetype: any
+    ): void => {
+      console.log(fieldname, file, filename, encoding, mimetype);
+
+      file.on("data", (data: any) => {
+        const lines = data.toString().split("\n");
+        lines.forEach((line: any) => {
+          const [studentId, studentName, sis, email] = line.split(",");
+          console.log(studentId, studentName, sis, email);
+          /* courseController.addStudentToCourse(
+            JSON.parse(formData.courseInfo).courseCode,
+            studentId,
+            studentName
+          ); */
+        });
+      });
+      file.resume();
+    }
+  );
+
+  bb.on("finish", async () => {
+    const {courseCode, courseName, room} = JSON.parse(formData.courseInfo);
+    try {
+      const response = await courseController.createCourse(
+        courseCode,
+        courseName,
+        room
+      );
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  req.on("error", (err) => {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  });
+
+  bb.end(req.body);
 };
 
 export const deleteCourseByCode = async (
