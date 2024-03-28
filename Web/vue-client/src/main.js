fdec6f6ab9bdf9a805938c71c1a2dd0bdf6eaade
strato67 <saaranshsharma28@gmail.com>
2024-03-28 01:13:06 -0400

    cleaning up unused files

--- Web/vue-client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-//import App from './App.vue'
-//import LogIn from "./LogIn"
-import MenuTab from "./components/MenuTab"
-//import MenuOpen from "./MenuOpen"
-//import Attendance from "./AttendancePage.vue"
-
-createApp(MenuTab).mount('#app')
